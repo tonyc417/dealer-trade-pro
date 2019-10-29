f45b04cdaf9fa2976d0432be42ab1102d5ebd6cd
unknown <tonycruz417@gmail.com>
2019-10-28 22:20:43 -0500

    added body parser

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
+
+
 const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 var PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log("Server started!"));
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(PORT, () => 
+console.log('Server started....'));
